e0e704efe91765cdf1a426bb9ffb6b4d4f230ee1
ssaghatelyan <saghatelyansusanna06@gmail.com>
2026-02-15 20:47:41 +0000

    Initial commit

---
+++ ft_print_string.c
@@ -0,0 +1,15 @@
+#include "ft_printf.h"
+
+int	print_str(char *str)
+{
+	int	len;
+
+	if (!str)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
+	len = ft_strlen(str);
+	ft_putstr_fd(str, 1);
+	return (len);
+}
