2f31793aef780817c890849170a0b5cc30cc6527
ssaghatelyan <saghatelyansusanna06@gmail.com>
2026-02-16 22:03:58 +0400

    Implement pointer printing function

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -1,1 +1,13 @@
+#include "ft_printf.h"
 
+int	ft_print_pointer(void *ptr)
+{
+	int				count;
+	unsigned long	address;
+
+	count = 0;
+	address = (unsigned long)ptr;
+	count += write(1, "0x", 2);
+	count += ft_print_hex(address, 'x');
+	return (count);
+}
