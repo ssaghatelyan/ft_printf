70c2cd0e355dafdcb61e4605f032ee9eabd34a43
ssaghatelyan <saghatelyansusanna06@gmail.com>
2026-02-16 22:02:20 +0400

    Add ft_print_int function for integer output
    Implement ft_print_int function to print integers.

--- ft_print_int.c
+++ ft_print_int.c
@@ -1,1 +1,21 @@
+#include "ft_printf.h"
 
+int ft_print_int(int n)
+{
+	long	num;
+	int	count;
+
+	num = n;
+	count = 0;
+	if (num < 0)
+	{
+		ft_putchar_fd('-', 1);
+		count++;
+		num = -num;
+	}
+	if (num >= 10)
+		count += ft_print_int(num / 10);
+	ft_putchar_fd((num % 10) + '0', 1);
+	count++;
+	return (count);
+}
