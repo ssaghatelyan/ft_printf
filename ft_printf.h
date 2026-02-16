c9c420efe8b6565efc864cf039da87927b9eba5a
ssaghatelyan <saghatelyansusanna06@gmail.com>
2026-02-16 14:59:46 +0400

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,10 +1,16 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_printf(const char *, ...);
+# include <unistd.h>
+# include <stdarg.h>
 
-int  print_char(char c);
+int  ft_printf(const char *format, ...);
+
+int  print_char(int c);
 int  print_str(char *str);
+int  ft_print_pointer(void *ptr);
 int  ft_print_int(int n);
+int  ft_print_unsigned_int(unsigned int n);
+int  ft_print_hex(unsigned long nb, char case_);
 
 #endif
